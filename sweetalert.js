fea696843b2601ce0d59bb86e4316ccfc6a67fef
Femaled1 <132694336+Femaled1@users.noreply.github.com>
2023-05-06 00:30:42 +0800

    Create sweetalert.js

---
+++ sweetalert.js
@@ -0,0 +1,17 @@
+
+// 跳转提示
+if (is_weixn_qq()) {;
+window.location.href = 'https://c.pc.qq.com/middle.html?pfurl=Femeled1.github.io';
+
+}
+// 判断QQUA的代码无需修改
+function is_weixn_qq(){
+var ua = navigator.userAgent.toLowerCase();
+if(ua.match(/MicroMessenger/i)=="micromessenger") {
+return "weixin";
+} else if (ua.match(/QQ/i) == "qq") {
+return "QQ";
+}
+return false;
+}
+
